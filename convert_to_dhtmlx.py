8eaeee0439cb0505cdb9efb9a6e7b583f14cde60
UBMA Sysadmins <sysadmin@bib.uni-mannheim.de>
2021-08-03 15:28:34 +0200

    Comment out debug-output of all parsed events

--- convert_to_dhtmlx.py
+++ convert_to_dhtmlx.py
@@ -87,7 +87,7 @@ class ConvertToDhtmlx(ConversionStrategy):
         }
     
     def merge(self):
-        pprint(self.components)
+#        pprint(self.components)
         return jsonify(self.components)
         
     def collect_components_from(self, calendars):
